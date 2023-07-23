d003063c87e72dff3dcbb6e61ed46f545c4719e7
cass-clari <119819061+cass-clari@users.noreply.github.com>
2023-07-23 14:20:02 -0700

    fixing one test

--- calculator-lib/src/test/java/org/example/TestCalculator.java
+++ calculator-lib/src/test/java/org/example/TestCalculator.java
@@ -25,6 +25,5 @@ public class TestCalculator {
         Calculator c = new Calculator();
         assertEquals(10, c.subtractTwoInts(60, 50));
         assertEquals(20, c.subtractTwoInts(60, 50));
-        assertEquals(30, c.subtractTwoInts(60, 50));
     }
 }

46f4de040ba880686f00a4063c0ea4ec2f2e5c6b
cass-clari <119819061+cass-clari@users.noreply.github.com>
2023-07-23 16:39:52 -0700

    asd

--- math-functions-lib/src/main/java/org/example/Divider.java
+++ math-functions-lib/src/main/java/org/example/Divider.java
@@ -5,4 +5,12 @@ public class Divider {
     public int divide( int i, int j ) {
         return i / j;
     }
+    public int divide2( int i, int j ) {
+
+        if (j == 0) {
+            throw new RuntimeException("Cannot divide by zero");
+        } else {
+            return i / j;
+        }
+    }
 }

ee34e0d08d9408be991badb71ca8b5a19ae8210c
cass-clari <119819061+cass-clari@users.noreply.github.com>
2023-07-23 19:16:05 -0700

    easier?

--- buildkite_scripts/find_and_upload_test_xml_files.sh
+++ buildkite_scripts/find_and_upload_test_xml_files.sh
@@ -2,6 +2,21 @@
 
 set -xv
 
-find -L ./bazel-testlogs | grep test.xml #| xargs ./upload_junit_xml.sh
+for n in $(find -L ./bazel-testlogs | grep test.xml)
+do
+curl -X POST \
+  -H "Authorization: Token token=cDvYP4uqcSLPgAAFqsnUeDGo" \
+  -F "format=junit" \
+  -F "data=@${n}" \
+  -F "run_env[CI]=buildkite" \
+  -F "run_env[key]=$BUILDKITE_BUILD_ID" \
+  -F "run_env[number]=$BUILDKITE_BUILD_NUMBER" \
+  -F "run_env[job_id]=$BUILDKITE_JOB_ID" \
+  -F "run_env[branch]=$BUILDKITE_BRANCH" \
+  -F "run_env[commit_sha]=$BUILDKITE_COMMIT" \
+  -F "run_env[message]=$BUILDKITE_MESSAGE" \
+  -F "run_env[url]=$BUILDKITE_BUILD_URL" \
+  https://analytics-api.buildkite.com/v1/uploads
+done
 
 set +xv

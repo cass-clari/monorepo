699b5e2c2d2d8819c3385c7a49a1db2c20b7970e
cass-clari <119819061+cass-clari@users.noreply.github.com>
2023-07-27 22:41:02 -0700

    L1 is back on the menu, boys!

---
+++ user-service/src/test/L1/java/service/server/TestServer.java
@@ -0,0 +1,53 @@
+package service.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import protos.user.EmailAddressOuterClass;
+import protos.user.UserOuterClass;
+import service.protos.LoginResponse;
+import service.protos.LoginUser;
+import io.grpc.testing.StreamRecorder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = UserServiceConfiguration.class)
+public class TestServer {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void testLoginResponse() throws Exception {
+        LoginUser request = LoginUser.newBuilder()
+                .setUsername("cass")
+                .setPwd("cass")
+                .build();
+        StreamRecorder<LoginResponse> responseObserver = StreamRecorder.create();
+        userService.login(request, responseObserver);
+        if (!responseObserver.awaitCompletion(5, TimeUnit.SECONDS)) {
+            fail("The call did not terminate in time");
+        }
+        assertNull(responseObserver.getError());
+        List<LoginResponse> results = responseObserver.getValues();
+        assertEquals(1, results.size());
+        LoginResponse response = results.get(0);
+
+
+        EmailAddressOuterClass.EmailAddress.Builder e = EmailAddressOuterClass.EmailAddress.newBuilder();
+        e.setEmail("cass@casslast.net");
+
+        UserOuterClass.User.Builder u = UserOuterClass.User.newBuilder();
+        u.setFirstName("cass").setLastName("casslast").setEmailAddress(e);
+
+        assertEquals(LoginResponse.newBuilder()
+                .setMessage("Hello there!").setUser(u.build()).setStatus(200)
+                .build(), response);
+    }
+}
---
+++ user-service/src/test/L1/java/service/server/UserDALStub.java
@@ -0,0 +1,18 @@
+package service.server;
+
+import protos.user.EmailAddressOuterClass;
+import protos.user.UserOuterClass;
+
+public class UserDALStub implements UserDAL {
+    @Override
+    public UserOuterClass.User validateUser(String username, String password) {
+        System.out.println("Inside STUB IMPL");
+        UserOuterClass.User.Builder u = UserOuterClass.User.newBuilder();
+        u.setFirstName(username).setLastName(username+"last");
+        EmailAddressOuterClass.EmailAddress.Builder email = EmailAddressOuterClass.EmailAddress.newBuilder();
+        email.setEmail(username + "@" + u.getLastName() + ".net");
+        u.setEmailAddress(email);
+
+        return u.build();
+    }
+}

22d36cc8c3d793239010e0ed1213a89ac8ec7b26
cass-clari <119819061+cass-clari@users.noreply.github.com>
2023-08-23 19:30:55 -0700

    no 404?

--- user-service/src/test/java/L0/service/server/TestUserService.java
+++ user-service/src/test/java/L0/service/server/TestUserService.java
@@ -26,7 +26,7 @@ public class TestUserService {
     @Autowired
     private UserService userService;
 
-    
+
     @Test
     public void testLoginResponse() throws Exception {
         LoginUser request = LoginUser.newBuilder()
